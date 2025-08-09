53ef37c606b90b41cc11302d74bcd2de38c08c33
Victor <daisel@gmail.com>
2025-08-09 21:09:49 +0400

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/android_apk/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android_apk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this)
+        textView.text = "Hello World"
+        setContentView(textView)
+    }
+}
